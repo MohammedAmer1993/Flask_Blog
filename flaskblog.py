5afba0d0707d889de3d95bea27f0977aa9c5e45d
Mohammed Amer <loard.amer.2010@gmail.com>
2024-03-30 01:49:47 +0200

    frist lesson is ok

---
+++ flaskblog.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route("/home")
+def hello_world():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def mo():
+    return render_template('about.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
